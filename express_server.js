278acff95912556b8566b52c4c3894eabd8af16d
Rida Batool <ridabatool.be13@gmail.com>
2023-04-18 02:39:25 -0400

    shortURL function implemented

--- express_server.js
+++ express_server.js
@@ -10,6 +10,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
