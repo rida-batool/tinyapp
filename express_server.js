6375d1f565f9542b5bd12a7ddb56ed7257624eff
Rida Batool <ridabatool.be13@gmail.com>
2023-04-19 13:41:40 -0400

    Logout button and cookie info cleared from browser

--- express_server.js
+++ express_server.js
@@ -3,8 +3,11 @@ var cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+//sets view engine ti ejs template
 app.set("view engine", "ejs");
+//this parses the req.body for html to read
 app.use(express.urlencoded({ extended: true }));
+//parses the cookie information stored in user's browser to pass with the get/post request
 app.use(cookieParser());
 
 const urlDatabase = {
@@ -12,11 +15,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//generates 6 alpha-numeric string
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 }
 
-//route handler for /urls and res.render() to pass the url data into out tempkate
+//route handler for /urls and res.render() to pass the url data into out template
 app.get("/urls", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
@@ -24,23 +28,26 @@ app.get("/urls", (req, res) => {
   };
   res.render("urls_index", templateVars);
 });
+//endpoint to go to page and add a new url to database
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"]
   };
   res.render("urls_new", templateVars);
 });
+//endpoint to post the new url to /urls
 app.post("/urls", (req, res) => {
   console.log(req.body);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
+//get request to go to main website page saved in shortURL
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
-
+//endpoint to go to shortURL page
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
@@ -67,6 +74,11 @@ app.post("/urls/:id/updated", (req, res) => {
 app.post("/login", (req, res) => {
   console.log(req.body.username);
   res.cookie('username', req.body.username);
+  res.redirect(`/urls`);
+});
+//logout endppoint triggered when user logout and cookie is cleared, user redirected to /urls
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect(`/urls`);
 });
 
